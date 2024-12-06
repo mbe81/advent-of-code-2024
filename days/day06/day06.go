f9d54e811605c115e28f389977b884f391c4b530
Marcel van den Berg <marcel@vandenberg.me>
2024-12-06 21:15:01 +0100

    Improved Day 06 - performance boost

--- days/day06/day06.go
+++ days/day06/day06.go
@@ -2,115 +2,132 @@ package day06
 
 import (
 	"fmt"
+	"time"
 )
 
-type Visit struct {
-	Position  Point
-	Direction Point
-}
-
-type Point struct {
-	Y int
-	X int
-}
-
-var (
-	Left  = Point{Y: 0, X: -1}
-	Right = Point{Y: 0, X: 1}
-	Up    = Point{Y: -1, X: 0}
-	Down  = Point{Y: 1, X: 0}
+const (
+	LEFT  int8 = 1
+	RIGHT int8 = 2
+	UP    int8 = 4
+	DOWN  int8 = 8
 )
 
 func part1(input []string) {
-	start := getStartingPosition(input)
-	visited, _ := solve(input, start, nil)
+	t := time.Now()
 
-	fmt.Println("Result Day 6, Part 1:", countUniqueVisits(visited))
+	startY, startX := getStartingPosition(input)
+	visited, _ := solve(input, startY, startX, -1, -1)
+
+	fmt.Println("Result Day 6, Part 1:", countVisits(visited), "Duration:", time.Since(t))
 }
 
 func part2(input []string) {
-	var loopCounter int
-	start := getStartingPosition(input)
+	t := time.Now()
 
+	startY, startX := getStartingPosition(input)
+	var loopCounter int
 	for y := range input {
 		for x := range input[y] {
 			if input[y][x] == '#' || input[y][x] == '^' {
 				continue
 			}
-
-			obstruction := Point{Y: y, X: x}
-
-			_, loop := solve(input, start, &obstruction)
+			_, loop := solve(input, startY, startX, int16(y), int16(x))
 			if loop {
 				loopCounter++
 			}
 		}
 	}
 
-	fmt.Println("Result Day 6, Part 2:", loopCounter)
+	fmt.Println("Result Day 6, Part 2:", loopCounter, "Duration:", time.Since(t))
 }
 
-func getStartingPosition(input []string) Point {
+func getStartingPosition(input []string) (int16, int16) {
 	for y := range input {
 		for x := range input[y] {
 			if input[y][x] == '^' {
-				return Point{Y: y, X: x}
+				return int16(y), int16(x)
 			}
 		}
 	}
-	return Point{}
+	return 0, 0
 }
 
-func solve(input []string, start Point, obstruction *Point) (map[Visit]bool, bool) {
-	pos := start
-	dir := Up
+func solve(input []string, startY, startX, obstrY, obstrX int16) ([][]int8, bool) {
+	var posY, posX, newY, newX, maxY, maxX int16
+	var dir int8
 
-	maxY := len(input) - 1
-	maxX := len(input[0]) - 1
+	posY = startY
+	posX = startX
+	maxY = int16(len(input) - 1)
+	maxX = int16(len(input[0]) - 1)
 
-	visited := make(map[Visit]bool)
-	visited[Visit{pos, dir}] = true
+	dir = UP
+
+	visited := make([][]int8, len(input))
+	for i := range visited {
+		visited[i] = make([]int8, len(input[i]))
+	}
 
 	loopDetected := false
 	for {
-		if pos.Y+dir.Y < 0 || pos.Y+dir.Y > maxY || pos.X+dir.X < 0 || pos.X+dir.X > maxX {
+		newY, newX = move(posY, posX, dir)
+		if newY < 0 || newY > maxY || newX < 0 || newX > maxX {
 			break
 		}
 		for {
-			if input[pos.Y+dir.Y][pos.X+dir.X] == '#' || (obstruction != nil && *obstruction == Point{pos.Y + dir.Y, pos.X + dir.X}) {
+			if input[newY][newX] == '#' || (newY == obstrY && newX == obstrX) {
 				dir = turnRight(dir)
+				newY, newX = move(posY, posX, dir)
 			} else {
 				break
 			}
 		}
-		pos.Y, pos.X = pos.Y+dir.Y, pos.X+dir.X
-		if visited[Visit{pos, dir}] {
+		posY, posX = newY, newX
+		if visited[posY][posX]&dir == dir {
 			loopDetected = true
 			break
 		}
-		visited[Visit{pos, dir}] = true
+		visited[posY][posX] = visited[posY][posX] | dir
 	}
 	return visited, loopDetected
 }
 
-func turnRight(dir Point) Point {
+func turnRight(dir int8) int8 {
 	switch dir {
-	case Up:
-		return Right
-	case Right:
-		return Down
-	case Down:
-		return Left
-	case Left:
-		return Up
+	case LEFT:
+		return UP
+	case RIGHT:
+		return DOWN
+	case DOWN:
+		return LEFT
+	case UP:
+		return RIGHT
 	}
-	return Point{}
+	return 0
 }
 
-func countUniqueVisits(visited map[Visit]bool) int {
-	visitedUnique := make(map[Point]bool)
-	for v := range visited {
-		visitedUnique[Point{Y: v.Position.Y, X: v.Position.X}] = true
+func move(posY, posX int16, dir int8) (int16, int16) {
+	switch dir {
+	case LEFT:
+		return posY, posX - 1
+	case RIGHT:
+		return posY, posX + 1
+	case UP:
+		return posY - 1, posX
+	case DOWN:
+		return posY + 1, posX
 	}
-	return len(visitedUnique)
+	return posY, posX
+}
+
+func countVisits(visited [][]int8) int {
+	var counter int
+	for y := range visited {
+		for x := range visited[y] {
+			if visited[y][x] > 0 {
+				counter++
+			}
+		}
+	}
+	return counter
 }
