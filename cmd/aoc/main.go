83275b1676083e47109e6cf19b78d283f083fe24
Marcel van den Berg <marcel@vandenberg.me>
2024-12-07 07:07:11 +0100

    Day 07

--- cmd/aoc/main.go
+++ cmd/aoc/main.go
@@ -10,6 +10,7 @@ import (
 	"github.com/mbe81/advent-of-code-2024/days/day04"
 	"github.com/mbe81/advent-of-code-2024/days/day05"
 	"github.com/mbe81/advent-of-code-2024/days/day06"
+	"github.com/mbe81/advent-of-code-2024/days/day07"
 )
 
 func main() {
@@ -38,6 +39,8 @@ func main() {
 		day05.Run(*part, *filename)
 	case 6:
 		day06.Run(*part, *filename)
+	case 7:
+		day07.Run(*part, *filename)
 	default:
 		fmt.Println("Invalid day. Please select a day between 1 and 5.")
 	}
---
+++ days/day07/day07.go
@@ -0,0 +1,100 @@
+package day07
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/mbe81/advent-of-code-2024/lib/convert"
+)
+
+func part1(input []string) {
+	t := time.Now()
+
+	testValues, numbers := parseInput(input)
+	operators := []string{"+", "*"}
+
+	var sumTestValues int
+	for i := range testValues {
+		lineOperators := generateOperators(operators, len(numbers[i])-1)
+		for j := range lineOperators {
+			calculatedValue := calculateValue(numbers[i], lineOperators[j], testValues[i])
+			if calculatedValue == testValues[i] {
+				sumTestValues += calculatedValue
+				break
+			}
+		}
+	}
+
+	fmt.Println("Result day 7, part 1:", sumTestValues, "- duration:", time.Since(t))
+}
+
+func part2(input []string) {
+	t := time.Now()
+
+	testValues, numbers := parseInput(input)
+	operators := []string{"+", "*", "|"}
+
+	var sumTestValues int
+	for i := range testValues {
+		lineOperators := generateOperators(operators, len(numbers[i])-1)
+		for j := range lineOperators {
+			calculatedValue := calculateValue(numbers[i], lineOperators[j], testValues[i])
+			if calculatedValue == testValues[i] {
+				sumTestValues += calculatedValue
+				break
+			}
+		}
+	}
+
+	fmt.Println("Result day 7, part 2:", sumTestValues, "- duration:", time.Since(t))
+}
+
+func parseInput(input []string) ([]int, [][]int) {
+	results, numbers := make([]int, len(input)), make([][]int, len(input))
+
+	for y := range input {
+		parts := strings.Split(input[y], ":")
+		results[y] = convert.StringToInt(parts[0])
+		numbers[y] = convert.LineToInts(parts[1], nil)
+	}
+
+	return results, numbers
+}
+
+func generateOperators(operators []string, length int) []string {
+	if length == 0 {
+		return []string{""}
+	}
+
+	subOperators := generateOperators(operators, length-1)
+	var result []string
+
+	for i := range operators {
+		for j := range subOperators {
+			result = append(result, operators[i]+subOperators[j])
+		}
+	}
+	return result
+}
+
+func calculateValue(numbers []int, operators string, testValue int) int {
+	result := numbers[0]
+
+	for i := 1; i < len(numbers); i++ {
+		if result > testValue {
+			return -1
+		}
+		switch operators[i-1] {
+		case '+':
+			result += numbers[i]
+		case '*':
+			result *= numbers[i]
+		case '|':
+			result = convert.StringToInt(strconv.Itoa(result) + strconv.Itoa(numbers[i]))
+		}
+	}
+
+	return result
+}
